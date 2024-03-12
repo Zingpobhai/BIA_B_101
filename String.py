621c7b30c614a6f8f9e5b40b37d6698722e33107
Zingpobhai <03230363.gcbs@rub.edu.bt>
2024-03-11 18:22:26 -0700

    String

---
+++ String.py
@@ -0,0 +1,25 @@
+# ! STRING MANIPULATION
+
+# ! string concatenation
+string1 = "2"
+string2 = "3"
+
+combined = string1 + string2
+combined1 = "hi" + " " + "there"
+
+print('String Concatenation')
+print(combined)
+print(combined1)
+
+# ! string interpolation
+print('String Interpolation')
+# ! how do you print "john is 30 years of age"
+name = "John"
+age = 99
+interpolated = f"John is {age} years of age"
+print(interpolated)
+
+# ! string methods
+print('String Methods')
+# # ! upper
+print('String upper')
