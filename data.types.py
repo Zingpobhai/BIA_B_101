b9945c429f3281c140ab95095c81e8258cd4302b
Zingpobhai <03230363.gcbs@rub.edu.bt>
2024-03-14 15:21:24 -0700

    dstatypesample.py

---
+++ data.types.py
@@ -0,0 +1,44 @@
+ #creation of array
+array1 = [1,2,3,"Thimphu",2.5]
+print(array1)
+
+#retriying
+element1 = array1[0]
+element2 = array1[4]
+lastElement = array1[-5]
+print(lastElement)
+print(element2)
+
+#modifying element
+array1[0] = 10
+print(array1)
+
+#getting no. of element in an array
+no_of_elements = len(array1)
+print(no_of_elements)
+
+#slicing
+elements = array1[0:2]
+print(elements)
+
+#print elements
+
+arr1 = [1,10]
+arr2 = ["Thimphu", "mongar"]
+no_to_check = 2
+result = number_to_check in arr1
+
+print(result)
+
+arrvariable = [1,2,3]
+arrvariable.append(4)
+print(arrvariable)
+
+#insert at a specific index
+arrvariable.insert9(1,10)
+print(arrvariable)
+arrvariable.sort()
+print(arrvariable)
+
+
+e
