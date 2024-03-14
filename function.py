4120ce939eb28fffcb47f4b596e17efcc37ed813
Zingpobhai <03230363.gcbs@rub.edu.bt>
2024-03-14 16:23:15 -0700

    function.py

---
+++ function.py
@@ -0,0 +1,7 @@
+def greet():
+    # do some calculation code 
+    # complex stuffs
+    print('Hello there')def greet():
+    # do some calculation code 
+    # complex stuffs
+    print('Hello there')
